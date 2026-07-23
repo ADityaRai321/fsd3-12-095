02b6d11a4802f0ea0fdad616ffe599d066264e0c
ADityaRai321 <rai.aditya0440@gmail.com>
2026-07-23 11:53:54 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,11 @@
+console.log("aditya rai");
+function sum (a,b){
+    return a+b;
+}
+const add=(a,b)=>{
+    return a+b;
+}; 
+const add2 = (a,b)=> a+b;
+console.log(sum(20,10));
+console.log(sum(20,10));
+console.log(sum(20,10));
